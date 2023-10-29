d3ab05e9c5397b69757e787a0da213b6ba1ef366
zhafaat <ilmuumum18@gmail.com>
2023-10-29 15:41:11 +0800

    linkkan sosial media

--- src/components/TopLeftImg.jsx
+++ src/components/TopLeftImg.jsx
@@ -3,8 +3,8 @@ import topLeftImg from '../assets/top-left-img.png'
 
 const TopLeftImg = () => {
   return (
-    <div className=' absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50'>
-        <img src={topLeftImg} width={400} height={400} alt="" />
+    <div className=' absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] brightness-90 opacity-50'>
+        <img src={topLeftImg} width={300} height={300} alt="" />
     </div>
   )
 }
