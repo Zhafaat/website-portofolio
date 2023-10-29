3334eadbe739c28c3c0fb29a65dda8bcb4cbb0c2
zhafaat <ilmuumum18@gmail.com>
2023-10-29 23:49:23 +0800

    styling bagian info page about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,92 @@
-import React from 'react'
+// icons
+import {
+    FaHtml5,
+    FaCss3,
+    FaJs,
+    FaReact,
+    FaWordpress,
+    FaFigma,
+  } from "react-icons/fa";
+  
+  import {
+    SiNextdotjs,
+    SiFramer,
+    SiAdobexd,
+    SiAdobephotoshop,
+  } from "react-icons/si";
+  
+  
+  //  data
+  const aboutData = [
+    {
+      title: 'skills',
+      info: [
+        {
+          title: 'Web Development',
+          icons: [
+            <FaHtml5 />,
+            <FaCss3 />,
+            <FaJs />,
+            <FaReact />,
+            <SiNextdotjs />,
+            <SiFramer />,
+            <FaWordpress />,
+          ],
+        },
+        {
+          title: 'UI/UX Design',
+          icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+        },
+      ],
+    },
+    {
+      title: 'awards',
+      info: [
+        {
+          title: 'Webby Awards - Honoree',
+          stage: '2011 - 2012',
+        },
+        {
+          title: 'Adobe Design Achievement Awards - Finalist',
+          stage: '2009 - 2010',
+        },
+      ],
+    },
+    {
+      title: 'experience',
+      info: [
+        {
+          title: 'UX/UI Designer - XYZ Company',
+          stage: '2012 - 2023',
+        },
+        {
+          title: 'Web Developer - ABC Agency',
+          stage: '2010 - 2012',
+        },
+        {
+          title: 'Intern - DEF Corporation',
+          stage: '2008 - 2010',
+        },
+      ],
+    },
+    {
+      title: 'credentials',
+      info: [
+        {
+          title: 'Web Development - ABC University, LA, CA',
+          stage: '2011',
+        },
+        {
+          title: 'Computer Science Diploma - AV Technical Institute',
+          stage: '2009',
+        },
+        {
+          title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+          stage: '2006',
+        },
+      ],
+    },
+  ];
 
 // components
 import Circles from '../components/Circles'
@@ -6,13 +94,18 @@ import Circles from '../components/Circles'
 // counter
 import CountUp from 'react-countup'
 
+// react
+import { useState } from "react";
+
 const About = () => {
+    const [index, setIndex] = useState(0)
+
   return (
     <div className=' h-full bg-primary/30 pt-[150px] lg:pt-[100px] text-center xl:text-left'>
         <Circles />
-        <div className=' container mx-auto w-[50%] h-full flex flex-col xl:flex-row gap-x-6 overflow-y-auto pb-20 xl:pb-10'>
+        <div className=' container mx-auto h-full flex flex-col xl:flex-row items-center justify-center gap-x-6 overflow-y-auto pb-20 xl:pb-5'>
             {/* text */}
-            <div className=' flex-1 flex flex-col'>
+            <div className='flex-1 flex flex-col'>
                 <h2 className=' h2'>
                     Captivating <span className=' text-accent'>strories</span> birdth magnificent designs.
                 </h2>
@@ -54,8 +147,38 @@ const About = () => {
                 </div>
             </div>
             {/* info */}
-            <div>
-
+            <div className=" flex flex-col w-full xl:max-w-[48%] h-[480px]">
+                <div className=" flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+                    {aboutData.map((item, itemIndex) => {
+                        return (
+                            <div 
+                                key={itemIndex}
+                                className={` ${index === itemIndex ? ' text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300' : ' after:w-8 after:bg-white'} cursor-pointer font-semibold capitalize xl:text-lg relative after:h-[2px] after:absolute after:-bottom-1 after:left-0`}
+                                onClick={() => setIndex(itemIndex)}
+                            >
+                                {item.title}
+                            </div>
+                        )
+                    })}
+                </div>
+                <div>
+                    {aboutData[index].info.map((item, itemIndex) => {
+                        return (
+                            <div key={itemIndex}>
+                                {/* title */}
+                                <div>{item.title}</div>
+                                <div>-</div>
+                                <div>{item.stage}</div>
+                                <div>
+                                    {/* icons */}
+                                    {item.icons?.map((icon, iconIndex) => {
+                                        return <div key={iconIndex}>{icon}</div>
+                                    })}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     <div className=" h-full bg-primary/60 m-0">
         {/* text */}
         <div className=" w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/30 pt-[150px] lg:pt-[100px] relative z-10">
-            <div className=" text-center flex flex-col xl:text-left h-full overflow-y-auto container mx-auto pb-24 xl:pb-5">
+            <div className=" flex flex-col overflow-y-auto text-center xl:text-left h-full container mx-auto pb-24 xl:pb-5">
                 {/* title  */}
                 <motion.h1
                     variants={fadeIn('down', 0.2)}
