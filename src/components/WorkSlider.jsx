62f79bb9106d9f58947d834212e68de2f7c08eee
zhafaat <ilmuumum18@gmail.com>
2023-10-31 21:27:00 +0800

    ganti data workSlider dengan data portofolio yang pernah dibuat

--- src/components/WorkSlider.jsx
+++ src/components/WorkSlider.jsx
@@ -1,3 +1,5 @@
+// assets
+
 // data
 const workSlides = {
     slides: [
@@ -5,19 +7,19 @@ const workSlides = {
         images: [
           {
             title: 'title',
-            path: '/thumb1.jpg',
+            path: 'thumb1.jpg',
           },
           {
             title: 'title',
-            path: '/thumb2.jpg',
+            path: 'thumb2.jpg',
           },
           {
             title: 'title',
-            path: '/thumb3.jpg',
+            path: 'thumb3.jpg',
           },
           {
             title: 'title',
-            path: '/thumb4.jpg',
+            path: 'thumb4.jpg',
           },
         ],
       },
@@ -25,19 +27,19 @@ const workSlides = {
         images: [
           {
             title: 'title',
-            path: '/thumb4.jpg',
+            path: 'thumb4.jpg',
           },
           {
             title: 'title',
-            path: '/thumb1.jpg',
+            path: 'thumb1.jpg',
           },
           {
             title: 'title',
-            path: '/thumb2.jpg',
+            path: 'thumb2.jpg',
           },
           {
             title: 'title',
-            path: '/thumb3.jpg',
+            path: 'thumb3.jpg',
           },
         ],
       },
@@ -47,32 +49,45 @@ const workSlides = {
 // swiper react components
 import { Swiper, SwiperSlide } from 'swiper/react'
 
+// import required modules 
+import { Pagination } from 'swiper/modules'
+
 // icons
 import { BsArrowRight } from 'react-icons/bs'
 
 const WorkSlider = () => {
   return (
-    <Swiper>
+    <Swiper
+        spaceBetween={10}
+        pagination={{
+            clickable: true
+        }}
+        modules={[Pagination]}
+        className=' h-[280px] sm:h-[480px]'
+    >
         {workSlides.slides.map((slide, slideIndex) => {
             return (
                 <SwiperSlide key={slideIndex}>
-                    <div>
+                    <div className=' grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer'>
                         {slide.images.map((image, imageIndex) => {
                             return (
-                                <div key={imageIndex}>
-                                    <div>
+                                <div 
+                                    key={imageIndex}
+                                    className=' relative rounded-lg  overflow-hidden flex items-center justify-center group'
+                                >
+                                    <div className=' flex items-center justify-center relative overflow-hidden group'>
                                         {/* image */}
-                                        <img src={image.path} alt="" />
-                                        <div></div>
+                                        <img src={image.path} width={500} height={300} alt="" />
+                                        <div className=' absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700'></div>
                                         {/* title */}
-                                        <div>
-                                            <div>
+                                        <div className=' absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
+                                            <div className=' flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
                                                 {/* title part 1 */}
-                                                <div>LIVE</div>
+                                                <div className=' delay-100'>LIVE</div>
                                                 {/* title part 2 */}
-                                                <div>PROJECT</div>
+                                                <div className=' translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
                                                 {/* icon */}
-                                                <div><BsArrowRight /></div>
+                                                <div className=' text-xl translate-y-[500%] text-xl group-hover:translate-y-0 transition-all duration-300 delay-200'><BsArrowRight /></div>
                                             </div>
                                         </div>
                                     </div>
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -6,21 +6,21 @@ import WorkSlider from '../components/WorkSlider'
 
 const Works = () => {
   return (
-    <div>
+    <div className=' h-full bg-primary/30'>
         <Circles />
-        <div>
-            <div>
+        <div className=' h-full container mx-auto pt-[150px] lg:pt-[100px]'>
+            <div className=' h-full flex flex-col items-center justify-center xl:flex-row gap-x-8 '>
                 {/* text */}
-                <div>
-                    <h2>
-                        My Work <span>.</span>
+                <div className=' text-center xl:text-left flex flex-col xl:w-[30vm] mb-4 xl:mb-0'>
+                    <h2 className=' h2 xl:mt-12'>
+                        My Work <span className=' text-accent'>.</span>
                     </h2>
-                    <p>
+                    <p className=' mb-4 max-w-[400px] mx-auto lg:mx-0'>
                     My portfolio illustrates my commitment to staying up-to-date with the latest web development technologies and best practices, resulting in modern and efficient websites.
                     </p>
                 </div>
                 {/* slider */}
-                <div>
+                <div className=' w-full xl:max-w-[65%]'>
                     <WorkSlider />
                 </div>
             </div>
