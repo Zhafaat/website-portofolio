cb30d4576782eff106f3e9d8d5b8cfd68e7d660b
zhafaat <ilmuumum18@gmail.com>
2023-11-01 14:17:41 +0800

    tambahkan framer motion di page testimonial

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+// components
+import TestimonialSlider from '../components/TestimonialSlider'
+
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div className=' h-full bg-primary/30 text-center'>
+      <div className=' container mx-auto h-full pt-[150px] xl:pt-[100px]'>
+        <div className=' h-full flex flex-col overflow-y-auto pb-0 sm:pb-20 xl:pb-0'>
+          <div className=' container m-auto '>
+            {/* title */}
+            <h2 className=' h2 mb-8 xl:mb-0'>
+              What clients <span>say.</span>
+            </h2>
+            {/* slider */}
+            <div>
+              <TestimonialSlider />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
